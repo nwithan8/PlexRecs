aa9c77bb103c2f0c1df694004094c53153901059
Nate Harris <nwithan8@users.noreply.github.com>
2021-01-10 03:16:19 -0700

    Added "Click here to view" link on embed

--- PlexRecs.py
+++ PlexRecs.py
@@ -37,13 +37,17 @@ def makeEmbed(mediaItem):
     imdb_item = imdb.get_imdb_item(mediaItem.title, analytics=analytics)
     embed = None
     if credentials.RETURN_PLEX_URL:
+        url = f"https://app.plex.tv/desktop#!/server/{plex.server_id}/details?key=%2Flibrary%2Fmetadata%2F{mediaItem.ratingKey}"
         embed = discord.Embed(title=mediaItem.title,
-                              url=f"https://app.plex.tv/desktop#!/server/{plex.server_id}/details?key=%2Flibrary%2Fmetadata%2F{mediaItem.ratingKey}",
+                              url=url,
                               description=f"Watch it on {credentials.PLEX_SERVER_NAME}")
+        embed.add_field(name="\u200b", value=f"[Click here to watch on Plex]({url})")
     else:
+        url = f"https://www.imdb.com/title/{imdb_item.imdb_id}"
         embed = discord.Embed(title=mediaItem.title,
-                              url=f"https://www.imdb.com/title/{imdb_item.imdb_id}",
+                              url=url,
                               description=f"View on IMDb")
+        embed.add_field(name="\u200b". value=f"[Click here to view on IMDb]({url})")
     embed.add_field(name="Summary", value=imdb_item.plot_outline, inline=False)
     embed.add_field(name="Release Date", value=imdb_item.release_date, inline=False)
     if mediaItem.type not in ['artist', 'album', 'track']:
