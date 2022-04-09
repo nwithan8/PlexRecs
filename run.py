226368cf7b8b66ad36096feab7349c9c25029909
Nate Harris <nharris@easypost.com>
2022-04-09 00:30:15 -0600

    - Remove dead code

--- run.py
+++ run.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
+
 import credentials
 from modules.logs import *
-import logging
 
 logging.basicConfig(format='%(levelname)s:%(message)s',
                     level=(logging.ERROR if credentials.SUPPRESS_LOGS else logging.INFO))
@@ -23,7 +23,8 @@ for ext in exts:
 @bot.event
 async def on_ready():
     info(f'\n\nLogged in as : {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
-    await bot.change_presence(status=discord.Status.idle, activity=discord.Game(name=f'in the REC league | {credentials.BOT_PREFIX}'))
+    await bot.change_presence(status=discord.Status.idle,
+                              activity=discord.Game(name=f'in the REC league | {credentials.BOT_PREFIX}'))
     info(f'Successfully logged in and booted...!\n')
 
 
